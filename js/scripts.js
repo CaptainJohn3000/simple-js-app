61c88ccc9959a2b7c2cc2048610c5b9fe9196963
Jonathan F <81685520+CaptainJohn3000@users.noreply.github.com>
2021-05-13 20:25:16 -0400

    Created Array, put pokemon objects in it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert("Hello World");
-
-let favoriteFood = "chicken";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 10,
+    type: ["grass", "poison"],
+  },
+  {
+    name: "Pikachu",
+    height: 4,
+    type: ["grass", "electric"],
+  },
+  {
+    name: "Charmander",
+    height: 5,
+    type: ["grass", "fire"],
+  },
+];
