8b4348f0efd68faec77f5378df348b6698b71d43
Jonathan F <81685520+CaptainJohn3000@users.noreply.github.com>
2021-06-07 05:59:14 -0400

    Update scripts.js
    Added functions inside data repository to load data from an external source (Added API)

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,20 @@
 // IIFE Array of Pokemon (objects)
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 0.7,
-      type: ["grass", "poison"],
-    },
-    {
-      name: "Squirtle",
-      height: 0.5,
-      type: ["water"],
-    },
-    {
-      name: "Charmander",
-      height: 0.6,
-      type: ["fire"],
-    },
-  ];
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
   // Add Pokemon to the Pokemon List
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (typeof pokemon === "object" && "name" in pokemon) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
 
   // Get all Pokemon from the Pokemon List
   function getAll() {
     return pokemonList;
-  }
-
-  // Print pokemon details to the console
-  function showDetails(pokemon) {
-    console.log(pokemon);
   }
 
   // Add buttons to the list items & add list items to unordered pokemon list
@@ -48,17 +32,62 @@ let pokemonRepository = (function () {
     });
   }
 
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  // Print pokemon details to the console
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
-    showDetails: showDetails,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
   };
 })();
 
-console.log(pokemonRepository.getAll());
-
-// Loop to iterate over array of Pokemon (objects) and add Pokemon to Pokemon List item
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
